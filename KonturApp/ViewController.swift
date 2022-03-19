2a3f1c38b8f6f96d975077268654cd0cdece57e0
nikita.dmitriev <nikita.dmitriev0314@yandex.ru>
2022-03-19 20:20:36 +0500

    Initial Commit

---
+++ KonturApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KonturApp
+//
+//  Created by Никита Дмитриев on 19.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
